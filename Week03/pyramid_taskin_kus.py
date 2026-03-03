893ee736d1f15f92464cf68a1218bca626873159
taskinkus27 <kustaskin@gmail.com>
2026-03-03 12:03:10 +0300

    Create pyramid_taskin_kus.py

---
+++ Week03/pyramid_taskin_kus.py
@@ -0,0 +1,8 @@
+def calculate_pyramid_height (number_of_blocks) :
+  height = 0
+  required_blocks = 1
+  while number_of_blocks >= required_blocks :
+    height += 1
+    number_of_blocks -= required_blocks
+    required_blocks += 1
+  return height
